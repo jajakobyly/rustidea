8599f290ee26f986ac52688b218befd44050ccf8
Marek Kaput <marek.kaput@outlook.com>
2015-08-21 14:57:28 +0200

    Highlight BAD_CHARACTERs and some minor polishments in Rust syntax highlighter.

--- src/org/rustidea/highlighting/RustSyntaxHighlighter.scala
+++ src/org/rustidea/highlighting/RustSyntaxHighlighter.scala
@@ -19,14 +19,19 @@ package org.rustidea.highlighting
 import com.intellij.lexer.Lexer
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey
-import com.intellij.openapi.editor.{DefaultLanguageHighlighterColors => DLHC}
+import com.intellij.openapi.editor.{DefaultLanguageHighlighterColors, HighlighterColors}
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase.{EMPTY, pack}
+import com.intellij.psi.TokenType
 import com.intellij.psi.tree.IElementType
 import org.rustidea.parser.RustLexer
-import org.rustidea.psi.{RustTypes => RT}
+import org.rustidea.psi.RustTypes
 
 import scala.collection.immutable.HashMap
+
+// FIXME Syntax highlighting for raw (byte) string breaks sometimes, I don't know when exactly and why.
+// FIXME An assertion fails in com.intellij.openapi.editor.ex.util.SegmentArray.segmentNotFound(SegmentArray.java:129) when trying to delete last quote character form raw (byte) string at the end of the file (making this operation impossible). I don't know why.
+// FIXME To sum up: raw (byte) string handling in lexer is fucked up.
 
 class RustSyntaxHighlighter extends SyntaxHighlighterBase {
   override def getHighlightingLexer: Lexer = new RustLexer()
@@ -38,81 +43,81 @@ class RustSyntaxHighlighter extends SyntaxHighlighterBase {
 object RustSyntaxHighlighter {
   final val IDENTIFIER = createTextAttributesKey(
     "RUST.IDENTIFIER",
-    DLHC.IDENTIFIER)
+    DefaultLanguageHighlighterColors.IDENTIFIER)
   final val KEYWORD = createTextAttributesKey(
     "RUST.KEYWORD",
-    DLHC.KEYWORD)
+    DefaultLanguageHighlighterColors.KEYWORD)
   final val OPERATION_SIGN = createTextAttributesKey(
     "RUST.OPERATION_SIGN",
-    DLHC.OPERATION_SIGN)
+    DefaultLanguageHighlighterColors.OPERATION_SIGN)
   final val BRACES = createTextAttributesKey(
     "RUST.BRACES",
-    DLHC.BRACES)
+    DefaultLanguageHighlighterColors.BRACES)
   final val DOT = createTextAttributesKey(
     "RUST.DOT",
-    DLHC.DOT)
+    DefaultLanguageHighlighterColors.DOT)
   final val SEMICOLON = createTextAttributesKey(
     "RUST.SEMICOLON",
-    DLHC.SEMICOLON)
+    DefaultLanguageHighlighterColors.SEMICOLON)
   final val COMMA = createTextAttributesKey(
     "RUST.COMMA",
-    DLHC.COMMA)
+    DefaultLanguageHighlighterColors.COMMA)
   final val PARENTHESES = createTextAttributesKey(
     "RUST.PARENTHESES",
-    DLHC.PARENTHESES)
+    DefaultLanguageHighlighterColors.PARENTHESES)
   final val BRACKETS = createTextAttributesKey(
     "RUST.BRACKETS",
-    DLHC.BRACKETS)
+    DefaultLanguageHighlighterColors.BRACKETS)
   final val LIFETIME = createTextAttributesKey(
     "RUST.LIFETIME",
-    DLHC.METADATA)
+    DefaultLanguageHighlighterColors.METADATA)
 
   final val NUMBER = createTextAttributesKey(
     "RUST.NUMBER",
-    DLHC.NUMBER)
+    DefaultLanguageHighlighterColors.NUMBER)
   final val CHAR = createTextAttributesKey(
     "RUST.CHAR",
-    DLHC.STRING)
+    DefaultLanguageHighlighterColors.STRING)
   final val STRING = createTextAttributesKey(
     "RUST.STRING",
-    DLHC.STRING)
+    DefaultLanguageHighlighterColors.STRING)
   final val RAW_STRING = createTextAttributesKey(
     "RUST.RAW_STRING",
-    DLHC.STRING)
+    DefaultLanguageHighlighterColors.STRING)
   final val BYTE = createTextAttributesKey(
     "RUST.BYTE",
-    DLHC.STRING)
+    DefaultLanguageHighlighterColors.STRING)
   final val BYTE_STRING = createTextAttributesKey(
     "RUST.BYTE_STRING",
-    DLHC.STRING)
+    DefaultLanguageHighlighterColors.STRING)
   final val RAW_BYTE_STRING = createTextAttributesKey(
     "RUST.RAW_BYTE_STRING",
-    DLHC.STRING)
+    DefaultLanguageHighlighterColors.STRING)
   final val VALID_ESCAPE = createTextAttributesKey(
     "RUST.VALID_ESCAPE",
-    DLHC.VALID_STRING_ESCAPE)
+    DefaultLanguageHighlighterColors.VALID_STRING_ESCAPE)
   final val INVALID_BYTE = createTextAttributesKey(
     "RUST.INVALID_BYTE",
-    DLHC.INVALID_STRING_ESCAPE)
+    DefaultLanguageHighlighterColors.INVALID_STRING_ESCAPE)
   final val INVALID_BYTE_STR_CHAR = createTextAttributesKey(
     "RUST.INVALID_BYTE_STR_CHAR",
-    DLHC.INVALID_STRING_ESCAPE)
+    DefaultLanguageHighlighterColors.INVALID_STRING_ESCAPE)
   final val INVALID_RAW_BYTE_STR_CHAR = createTextAttributesKey(
     "RUST.INVALID_RAW_BYTE_STR_CHAR",
-    DLHC.INVALID_STRING_ESCAPE)
+    DefaultLanguageHighlighterColors.INVALID_STRING_ESCAPE)
 
   final val BLOCK_COMMENT = createTextAttributesKey(
     "RUST.BLOCK_COMMENT",
-    DLHC.BLOCK_COMMENT)
+    DefaultLanguageHighlighterColors.BLOCK_COMMENT)
   final val LINE_COMMENT = createTextAttributesKey(
     "RUST.LINE_COMMENT",
-    DLHC.LINE_COMMENT)
+    DefaultLanguageHighlighterColors.LINE_COMMENT)
   final val DOC_BLOCK_COMMENT = createTextAttributesKey(
     "RUST.DOC_BLOCK_COMMENT",
-    DLHC.DOC_COMMENT)
+    DefaultLanguageHighlighterColors.DOC_COMMENT)
   final val DOC_LINE_COMMENT = createTextAttributesKey(
     "RUST.DOC_LINE_COMMENT",
-    DLHC.DOC_COMMENT)
+    DefaultLanguageHighlighterColors.DOC_COMMENT)
   final val PARENT_DOC_BLOCK_COMMENT = createTextAttributesKey(
     "RUST.PARENT_DOC_BLOCK_COMMENT",
     DOC_BLOCK_COMMENT)
@@ -120,136 +125,142 @@ object RustSyntaxHighlighter {
     "RUST.PARENT_DOC_LINE_COMMENT",
     DOC_LINE_COMMENT)
 
+  final val BAD_CHARACTER = createTextAttributesKey(
+    "RUST.BAD_CHARACTER",
+    HighlighterColors.BAD_CHARACTER)
+
   private final val KEYS = HashMap(
-    RT.IDENTIFIER -> IDENTIFIER,
+    RustTypes.IDENTIFIER -> IDENTIFIER,
 
-    RT.KW_ABSTRACT -> KEYWORD,
-    RT.KW_ALIGNOF -> KEYWORD,
-    RT.KW_AS -> KEYWORD,
-    RT.KW_BECOME -> KEYWORD,
-    RT.KW_BOX -> KEYWORD,
-    RT.KW_BREAK -> KEYWORD,
-    RT.KW_CONST -> KEYWORD,
-    RT.KW_CONTINUE -> KEYWORD,
-    RT.KW_CRATE -> KEYWORD,
-    RT.KW_DO -> KEYWORD,
-    RT.KW_ELSE -> KEYWORD,
-    RT.KW_ENUM -> KEYWORD,
-    RT.KW_EXTERN -> KEYWORD,
-    RT.KW_FALSE -> KEYWORD,
-    RT.KW_FINAL -> KEYWORD,
-    RT.KW_FN -> KEYWORD,
-    RT.KW_FOR -> KEYWORD,
-    RT.KW_IF -> KEYWORD,
-    RT.KW_IMPL -> KEYWORD,
-    RT.KW_IN -> KEYWORD,
-    RT.KW_LET -> KEYWORD,
-    RT.KW_LOOP -> KEYWORD,
-    RT.KW_MACRO -> KEYWORD,
-    RT.KW_MATCH -> KEYWORD,
-    RT.KW_MOD -> KEYWORD,
-    RT.KW_MOVE -> KEYWORD,
-    RT.KW_MUT -> KEYWORD,
-    RT.KW_OFFSETOF -> KEYWORD,
-    RT.KW_OVERRIDE -> KEYWORD,
-    RT.KW_PRIV -> KEYWORD,
-    RT.KW_PROC -> KEYWORD,
-    RT.KW_PUB -> KEYWORD,
-    RT.KW_PURE -> KEYWORD,
-    RT.KW_REF -> KEYWORD,
-    RT.KW_RETURN -> KEYWORD,
-    RT.KW_SELF_T -> KEYWORD,
-    RT.KW_SELF -> KEYWORD,
-    RT.KW_SIZEOF -> KEYWORD,
-    RT.KW_STATIC -> KEYWORD,
-    RT.KW_STRUCT -> KEYWORD,
-    RT.KW_SUPER -> KEYWORD,
-    RT.KW_TRAIT -> KEYWORD,
-    RT.KW_TRUE -> KEYWORD,
-    RT.KW_TYPE -> KEYWORD,
-    RT.KW_TYPEOF -> KEYWORD,
-    RT.KW_UNSAFE -> KEYWORD,
-    RT.KW_UNSIZED -> KEYWORD,
-    RT.KW_USE -> KEYWORD,
-    RT.KW_VIRTUAL -> KEYWORD,
-    RT.KW_WHERE -> KEYWORD,
-    RT.KW_WHILE -> KEYWORD,
-    RT.KW_YIELD -> KEYWORD,
+    RustTypes.KW_ABSTRACT -> KEYWORD,
+    RustTypes.KW_ALIGNOF -> KEYWORD,
+    RustTypes.KW_AS -> KEYWORD,
+    RustTypes.KW_BECOME -> KEYWORD,
+    RustTypes.KW_BOX -> KEYWORD,
+    RustTypes.KW_BREAK -> KEYWORD,
+    RustTypes.KW_CONST -> KEYWORD,
+    RustTypes.KW_CONTINUE -> KEYWORD,
+    RustTypes.KW_CRATE -> KEYWORD,
+    RustTypes.KW_DO -> KEYWORD,
+    RustTypes.KW_ELSE -> KEYWORD,
+    RustTypes.KW_ENUM -> KEYWORD,
+    RustTypes.KW_EXTERN -> KEYWORD,
+    RustTypes.KW_FALSE -> KEYWORD,
+    RustTypes.KW_FINAL -> KEYWORD,
+    RustTypes.KW_FN -> KEYWORD,
+    RustTypes.KW_FOR -> KEYWORD,
+    RustTypes.KW_IF -> KEYWORD,
+    RustTypes.KW_IMPL -> KEYWORD,
+    RustTypes.KW_IN -> KEYWORD,
+    RustTypes.KW_LET -> KEYWORD,
+    RustTypes.KW_LOOP -> KEYWORD,
+    RustTypes.KW_MACRO -> KEYWORD,
+    RustTypes.KW_MATCH -> KEYWORD,
+    RustTypes.KW_MOD -> KEYWORD,
+    RustTypes.KW_MOVE -> KEYWORD,
+    RustTypes.KW_MUT -> KEYWORD,
+    RustTypes.KW_OFFSETOF -> KEYWORD,
+    RustTypes.KW_OVERRIDE -> KEYWORD,
+    RustTypes.KW_PRIV -> KEYWORD,
+    RustTypes.KW_PROC -> KEYWORD,
+    RustTypes.KW_PUB -> KEYWORD,
+    RustTypes.KW_PURE -> KEYWORD,
+    RustTypes.KW_REF -> KEYWORD,
+    RustTypes.KW_RETURN -> KEYWORD,
+    RustTypes.KW_SELF_T -> KEYWORD,
+    RustTypes.KW_SELF -> KEYWORD,
+    RustTypes.KW_SIZEOF -> KEYWORD,
+    RustTypes.KW_STATIC -> KEYWORD,
+    RustTypes.KW_STRUCT -> KEYWORD,
+    RustTypes.KW_SUPER -> KEYWORD,
+    RustTypes.KW_TRAIT -> KEYWORD,
+    RustTypes.KW_TRUE -> KEYWORD,
+    RustTypes.KW_TYPE -> KEYWORD,
+    RustTypes.KW_TYPEOF -> KEYWORD,
+    RustTypes.KW_UNSAFE -> KEYWORD,
+    RustTypes.KW_UNSIZED -> KEYWORD,
+    RustTypes.KW_USE -> KEYWORD,
+    RustTypes.KW_VIRTUAL -> KEYWORD,
+    RustTypes.KW_WHERE -> KEYWORD,
+    RustTypes.KW_WHILE -> KEYWORD,
+    RustTypes.KW_YIELD -> KEYWORD,
 
-    RT.OP_AMPERSAND -> OPERATION_SIGN,
-    RT.OP_ARROW -> OPERATION_SIGN,
-    RT.OP_ASSIGN -> OPERATION_SIGN,
-    RT.OP_ASTERISK -> OPERATION_SIGN,
-    RT.OP_AT -> OPERATION_SIGN,
-    RT.OP_CARET -> OPERATION_SIGN,
-    RT.OP_CLOSE_BRACE -> BRACES,
-    RT.OP_CLOSE_BRACKET -> BRACKETS,
-    RT.OP_CLOSE_PAREN -> PARENTHESES,
-    RT.OP_COLON -> OPERATION_SIGN,
-    RT.OP_COMMA -> COMMA,
-    RT.OP_DOLLAR -> OPERATION_SIGN,
-    RT.OP_DOT -> DOT,
-    RT.OP_DOUBLE_AMPERSAND -> OPERATION_SIGN,
-    RT.OP_DOUBLE_COLON -> OPERATION_SIGN,
-    RT.OP_DOUBLE_DOT -> OPERATION_SIGN,
-    RT.OP_DOUBLE_PIPE -> OPERATION_SIGN,
-    RT.OP_EQUALS -> OPERATION_SIGN,
-    RT.OP_EXCLAMATION_MARK -> OPERATION_SIGN,
-    RT.OP_FAT_ARROW -> OPERATION_SIGN,
-    RT.OP_GREATER_THAN -> OPERATION_SIGN,
-    RT.OP_GREATER_THAN_EQ -> OPERATION_SIGN,
-    RT.OP_HASH -> OPERATION_SIGN,
-    RT.OP_LEFT_SHIFT -> OPERATION_SIGN,
-    RT.OP_LESS_THAN -> OPERATION_SIGN,
-    RT.OP_LESS_THAN_EQ -> OPERATION_SIGN,
-    RT.OP_MINUS -> OPERATION_SIGN,
-    RT.OP_NOT_EQUALS -> OPERATION_SIGN,
-    RT.OP_OPEN_BRACE -> BRACES,
-    RT.OP_OPEN_BRACKET -> BRACKETS,
-    RT.OP_OPEN_PAREN -> PARENTHESES,
-    RT.OP_PERCENT -> OPERATION_SIGN,
-    RT.OP_PIPE -> OPERATION_SIGN,
-    RT.OP_PLUS -> OPERATION_SIGN,
-    RT.OP_RIGHT_SHIFT -> OPERATION_SIGN,
-    RT.OP_SEMICOLON -> SEMICOLON,
-    RT.OP_SLASH -> OPERATION_SIGN,
-    RT.OP_TRIPLE_DOT -> OPERATION_SIGN,
-    RT.OP_UNDERSCORE -> OPERATION_SIGN,
+    RustTypes.OP_AMPERSAND -> OPERATION_SIGN,
+    RustTypes.OP_ARROW -> OPERATION_SIGN,
+    RustTypes.OP_ASSIGN -> OPERATION_SIGN,
+    RustTypes.OP_ASTERISK -> OPERATION_SIGN,
+    RustTypes.OP_AT -> OPERATION_SIGN,
+    RustTypes.OP_CARET -> OPERATION_SIGN,
+    RustTypes.OP_CLOSE_BRACE -> BRACES,
+    RustTypes.OP_CLOSE_BRACKET -> BRACKETS,
+    RustTypes.OP_CLOSE_PAREN -> PARENTHESES,
+    RustTypes.OP_COLON -> OPERATION_SIGN,
+    RustTypes.OP_COMMA -> COMMA,
+    RustTypes.OP_DOLLAR -> OPERATION_SIGN,
+    RustTypes.OP_DOT -> DOT,
+    RustTypes.OP_DOUBLE_AMPERSAND -> OPERATION_SIGN,
+    RustTypes.OP_DOUBLE_COLON -> OPERATION_SIGN,
+    RustTypes.OP_DOUBLE_DOT -> OPERATION_SIGN,
+    RustTypes.OP_DOUBLE_PIPE -> OPERATION_SIGN,
+    RustTypes.OP_EQUALS -> OPERATION_SIGN,
+    RustTypes.OP_EXCLAMATION_MARK -> OPERATION_SIGN,
+    RustTypes.OP_FAT_ARROW -> OPERATION_SIGN,
+    RustTypes.OP_GREATER_THAN -> OPERATION_SIGN,
+    RustTypes.OP_GREATER_THAN_EQ -> OPERATION_SIGN,
+    RustTypes.OP_HASH -> OPERATION_SIGN,
+    RustTypes.OP_LEFT_SHIFT -> OPERATION_SIGN,
+    RustTypes.OP_LESS_THAN -> OPERATION_SIGN,
+    RustTypes.OP_LESS_THAN_EQ -> OPERATION_SIGN,
+    RustTypes.OP_MINUS -> OPERATION_SIGN,
+    RustTypes.OP_NOT_EQUALS -> OPERATION_SIGN,
+    RustTypes.OP_OPEN_BRACE -> BRACES,
+    RustTypes.OP_OPEN_BRACKET -> BRACKETS,
+    RustTypes.OP_OPEN_PAREN -> PARENTHESES,
+    RustTypes.OP_PERCENT -> OPERATION_SIGN,
+    RustTypes.OP_PIPE -> OPERATION_SIGN,
+    RustTypes.OP_PLUS -> OPERATION_SIGN,
+    RustTypes.OP_RIGHT_SHIFT -> OPERATION_SIGN,
+    RustTypes.OP_SEMICOLON -> SEMICOLON,
+    RustTypes.OP_SLASH -> OPERATION_SIGN,
+    RustTypes.OP_TRIPLE_DOT -> OPERATION_SIGN,
+    RustTypes.OP_UNDERSCORE -> OPERATION_SIGN,
 
-    RT.LIFETIME -> LIFETIME,
+    RustTypes.LIFETIME -> LIFETIME,
 
-    RT.DEC_LIT -> NUMBER,
-    RT.BIN_LIT -> NUMBER,
-    RT.OCT_LIT -> NUMBER,
-    RT.HEX_LIT -> NUMBER,
-    RT.FLOAT_LIT -> NUMBER,
+    RustTypes.DEC_LIT -> NUMBER,
+    RustTypes.BIN_LIT -> NUMBER,
+    RustTypes.OCT_LIT -> NUMBER,
+    RustTypes.HEX_LIT -> NUMBER,
+    RustTypes.FLOAT_LIT -> NUMBER,
 
-    RT.CHAR_LIT -> CHAR,
-    RT.BYTE_LIT -> BYTE,
-    RT.STR_LIT_BEGIN -> STRING,
-    RT.STR_LIT_END -> STRING,
-    RT.STR_LIT_TOKEN -> STRING,
-    RT.STR_LIT_ESCAPE -> VALID_ESCAPE,
-    RT.RAW_STR_LIT_BEGIN -> RAW_STRING,
-    RT.RAW_STR_LIT_END -> RAW_STRING,
-    RT.RAW_STR_LIT_TOKEN -> RAW_STRING,
-    RT.BYTE_STR_LIT_BEGIN -> BYTE_STRING,
-    RT.BYTE_STR_LIT_END -> BYTE_STRING,
-    RT.BYTE_STR_LIT_TOKEN -> BYTE_STRING,
-    RT.BYTE_STR_LIT_ESCAPE -> VALID_ESCAPE,
-    RT.RAW_BYTE_STR_LIT_BEGIN -> RAW_BYTE_STRING,
-    RT.RAW_BYTE_STR_LIT_END -> RAW_BYTE_STRING,
-    RT.RAW_BYTE_STR_LIT_TOKEN -> RAW_BYTE_STRING,
+    RustTypes.CHAR_LIT -> CHAR,
+    RustTypes.BYTE_LIT -> BYTE,
+    RustTypes.STR_LIT_BEGIN -> STRING,
+    RustTypes.STR_LIT_END -> STRING,
+    RustTypes.STR_LIT_TOKEN -> STRING,
+    RustTypes.STR_LIT_ESCAPE -> VALID_ESCAPE,
+    RustTypes.RAW_STR_LIT_BEGIN -> RAW_STRING,
+    RustTypes.RAW_STR_LIT_END -> RAW_STRING,
+    RustTypes.RAW_STR_LIT_TOKEN -> RAW_STRING,
+    RustTypes.BYTE_STR_LIT_BEGIN -> BYTE_STRING,
+    RustTypes.BYTE_STR_LIT_END -> BYTE_STRING,
+    RustTypes.BYTE_STR_LIT_TOKEN -> BYTE_STRING,
+    RustTypes.BYTE_STR_LIT_ESCAPE -> VALID_ESCAPE,
+    RustTypes.RAW_BYTE_STR_LIT_BEGIN -> RAW_BYTE_STRING,
+    RustTypes.RAW_BYTE_STR_LIT_END -> RAW_BYTE_STRING,
+    RustTypes.RAW_BYTE_STR_LIT_TOKEN -> RAW_BYTE_STRING,
 
-    RT.INVALID_BYTE_LIT -> INVALID_BYTE,
-    RT.INVALID_BYTE_STR_LIT_TOKEN -> INVALID_BYTE_STR_CHAR,
-    RT.INVALID_RAW_BYTE_STR_LIT_TOKEN -> INVALID_RAW_BYTE_STR_CHAR,
+    RustTypes.INVALID_BYTE_LIT -> INVALID_BYTE,
+    RustTypes.INVALID_BYTE_STR_LIT_TOKEN -> INVALID_BYTE_STR_CHAR,
+    RustTypes.INVALID_RAW_BYTE_STR_LIT_TOKEN -> INVALID_RAW_BYTE_STR_CHAR,
 
-    RT.BLOCK_COMMENT -> BLOCK_COMMENT,
-    RT.LINE_COMMENT -> LINE_COMMENT,
-    RT.DOC_BLOCK_COMMENT -> DOC_BLOCK_COMMENT,
-    RT.DOC_LINE_COMMENT -> DOC_LINE_COMMENT,
-    RT.PARENT_DOC_BLOCK_COMMENT -> PARENT_DOC_BLOCK_COMMENT,
-    RT.PARENT_DOC_LINE_COMMENT -> PARENT_DOC_LINE_COMMENT
+    RustTypes.BLOCK_COMMENT -> BLOCK_COMMENT,
+    RustTypes.LINE_COMMENT -> LINE_COMMENT,
+    RustTypes.DOC_BLOCK_COMMENT -> DOC_BLOCK_COMMENT,
+    RustTypes.DOC_LINE_COMMENT -> DOC_LINE_COMMENT,
+    RustTypes.PARENT_DOC_BLOCK_COMMENT -> PARENT_DOC_BLOCK_COMMENT,
+    RustTypes.PARENT_DOC_LINE_COMMENT -> PARENT_DOC_LINE_COMMENT,
+
+    TokenType.BAD_CHARACTER -> BAD_CHARACTER
   )
 }
