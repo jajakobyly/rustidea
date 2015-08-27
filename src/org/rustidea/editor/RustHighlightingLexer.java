609912b72c01226c26c41c64998e6e581f0bba74
Marek Kaput <marek.kaput@outlook.com>
2015-08-27 16:20:32 +0200

    Highlight escapes in literals using layered lexers.

--- src/org/rustidea/editor/RustHighlightingLexer.java
+++ src/org/rustidea/editor/RustHighlightingLexer.java
@@ -17,10 +17,33 @@
 package org.rustidea.editor;
 
 import com.intellij.lexer.LayeredLexer;
+import com.intellij.psi.tree.IElementType;
 import org.rustidea.parser.RustLexer;
+import org.rustidea.parser.RustStringLiteralLexer;
+import org.rustidea.psi.RustTypes;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.rustidea.parser.RustStringLiteralLexer.ESCAPE.*;
 
 public class RustHighlightingLexer extends LayeredLexer {
+    private static final Map<IElementType, EnumSet<RustStringLiteralLexer.ESCAPE>> STRING_LAYERS = new HashMap<IElementType, EnumSet<RustStringLiteralLexer.ESCAPE>>();
+
+    static {
+        STRING_LAYERS.put(RustTypes.CHAR_LIT, EnumSet.of(BYTE_ESCAPE, UNICODE_ESCAPE));
+        STRING_LAYERS.put(RustTypes.BYTE_LIT, EnumSet.of(BYTE_ESCAPE));
+        STRING_LAYERS.put(RustTypes.STRING_LIT, EnumSet.of(BYTE_ESCAPE, UNICODE_ESCAPE, EOL_ESCAPE));
+        STRING_LAYERS.put(RustTypes.BYTE_STRING_LIT, EnumSet.of(BYTE_ESCAPE, EOL_ESCAPE));
+    }
+
     public RustHighlightingLexer() {
         super(new RustLexer());
+        for (Map.Entry<IElementType, EnumSet<RustStringLiteralLexer.ESCAPE>> layer : STRING_LAYERS.entrySet()) {
+            IElementType literal = layer.getKey();
+            EnumSet<RustStringLiteralLexer.ESCAPE> escapes = layer.getValue();
+            registerLayer(new RustStringLiteralLexer(literal, escapes), literal);
+        }
     }
 }
